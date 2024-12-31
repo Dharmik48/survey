26831ceac8cfc0bae618b377b668e2e47bfb6a19
dharmik48 <hingudharmik4@gmail.com>
2024-12-31 22:46:02 +0530

    feat: adds home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,29 @@
+import { Button } from '@nextui-org/button'
+import { BackgroundBeams } from './components/ui/background-beams'
+
 function App() {
-	return <h1>Vite + React</h1>
+	return (
+		<div className='h-dvh flex'>
+			<BackgroundBeams />
+			<div className='h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center my-auto'>
+				<div className='max-w-2xl mx-auto p-4 text-center space-y-8'>
+					<h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
+						Discover What Matters with{' '}
+						<span className='bg-gradient-to-b from-background via-primary to-primary bg-clip-text text-transparent tracking-tighter'>
+							SurveySphere
+						</span>
+					</h1>
+					<p className='leading-7 [&:not(:first-child)]:mt-6'>
+						Build and share surveys that uncover insights, helping you make
+						informed decisions.
+					</p>
+					<Button color='primary' size='lg'>
+						Get Started
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
