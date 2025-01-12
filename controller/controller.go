c47e8f79cc663798aea584f2dda272d818081f88
dharmik48 <hingudharmik4@gmail.com>
2025-01-12 20:39:07 +0530

    feat: adds register logic

--- controller/controller.go
+++ controller/controller.go
@@ -3,11 +3,13 @@ package controller
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type User struct {
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Email string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=32"`
 }
 
 func Signup(w http.ResponseWriter, r *http.Request) {
@@ -18,18 +20,21 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: add validation
-	if user.Email == "" || user.Password == "" {
-		http.Error(w, "Missing Data.", http.StatusBadRequest)
+	// validation
+	validate := validator.New()
+
+	if err := validate.Struct(user); err != nil {
+		http.Error(w, "Failed to validate data", http.StatusBadRequest)
 		return
 	}
 
-	json, err := json.Marshal(user)
+	json, err := json.Marshal("Success")
 
 	if err != nil {
 		http.Error(w, "Could not convert response to JSON.", http.StatusInternalServerError)
 		return
 	}
 
+	w.WriteHeader(http.StatusCreated)
 	w.Write(json)
 }
