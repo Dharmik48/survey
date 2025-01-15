68588ba0c34e2e064c1d030f4580a4a814651859
dharmik48 <hingudharmik4@gmail.com>
2025-01-15 22:08:45 +0530

    feat: adds better api response

--- server/api/user.go
+++ server/api/user.go
@@ -7,16 +7,28 @@ import (
 
 	"github.com/dharmik48/survey/database"
 	"github.com/dharmik48/survey/models"
+	"github.com/dharmik48/survey/types"
 	"github.com/dharmik48/survey/utils"
 	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
+func Error(w http.ResponseWriter, message string, code int) {
+	res := types.Response{
+		Status: types.Error,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(res)
+}
+
 func Signup(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		http.Error(w, "Failed to decode JSON.", http.StatusBadRequest)
+		Error(w, "Failed to decode JSON.", http.StatusBadRequest)
 		return
 	}
 
@@ -24,35 +36,35 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 	validate := validator.New()
 
 	if err := validate.Struct(user); err != nil {
-		http.Error(w, "Failed to validate data.", http.StatusBadRequest)
+		Error(w, "Failed to validate data.", http.StatusBadRequest)
 		return
 	}
 
 	// hash password
 	if err := utils.Hash(&user.Password); err != nil {
-		http.Error(w, "Failed to hash password.", http.StatusInternalServerError)
+		Error(w, "Failed to hash password.", http.StatusInternalServerError)
 		return
 	}
 
 	result := database.DB.Create(&user)
 
 	if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
-		http.Error(w, "Email already exists.", http.StatusConflict)
+		Error(w, "Email already exists.", http.StatusConflict)
 		return
 	}
 
 	if result.Error != nil {
-		http.Error(w, "Could not create user.", http.StatusInternalServerError)
+		Error(w, "Could not create user.", http.StatusInternalServerError)
 		return
 	}
 
-	json, err := json.Marshal(user.ID)
-
-	if err != nil {
-		http.Error(w, "Could not convert response to JSON.", http.StatusInternalServerError)
-		return
+	res := types.Response{
+		Status: types.Success,
+		Message: "Registration successful",
+		Data: map[string]any{"id": user.ID },
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	w.Write(json)
+	json.NewEncoder(w).Encode(res)
 }
